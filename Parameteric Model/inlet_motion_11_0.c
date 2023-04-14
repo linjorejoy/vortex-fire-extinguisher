677d77a28e8cbe105973a6e987876e6e3ed675d9
Linjo Rejoy <howyoudoing1618@gmail.com>
2023-04-14 11:15:01 +0100

    init commit

---
+++ Parameteric Model/inlet_motion_11_0.c
@@ -0,0 +1,14 @@
+#include "udf.h"
+
+DEFINE_PROFILE(unsteady_velocity,thread,position)
+{
+face_t f;
+real t=CURRENT_TIME;
+
+begin_f_loop(f,thread)
+	{
+	F_PROFILE(f,thread,position)=9.42*sin(30*t);
+	}
+
+end_f_loop(f,thread)
+}
